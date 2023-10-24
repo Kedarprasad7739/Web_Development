7d5048048cb0f2fac57f5a1cc99ee7830778e67a
Kedar Prasad <mr.kedarprasad@gmail.com>
2023-10-24 13:27:29 +0530

    every variable

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ let odd=[1,3,5,7,9]
 let even=[2,4,6,8,10]
 let nums=[...odd,...even]
 console.log(nums);
+let number=[2,1,5,7,8,12]
+let every=number.every((el) => {
+    return el%2==0;
+})
+console.log(every);
